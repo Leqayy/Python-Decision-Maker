ae9393c9deaab7474d39c721fee6585004e751d2
Leqayy <99774742+Leqayy@users.noreply.github.com>
2022-02-16 01:04:31 +0000

    Create code.py

---
+++ code.py
@@ -0,0 +1,8 @@
+thing1 = input("what is the first thing you want to decide on: ")
+thing2 = input("what is the second thing you want to decide on: ")
+
+result = number
+if result == 1:
+    print("your result is " + thing1)
+elif result == 2:
+    print("your result is " + thing2)
